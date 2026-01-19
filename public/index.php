9543a96c7690b5ce61828d62f3d7fe5e5c80d26c
glerique <gael.lerique@gmail.com>
2026-01-19 19:41:38 +0100

    feat: Update entry point to use Kernel

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
-use NesCore\Http\Request;
-use NesCore\Http\Response;
-
 declare(strict_types=1);
+
+use NesCore\Http\Kernel;
+use NesCore\Http\Request;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$content = '<h1>Hello World</h1>';
 
-$response = new Response(content: $content, status: 200, headers: []);
+$kernel = new Kernel();
+$response = $kernel->handle($request);
 
 $response->send();
